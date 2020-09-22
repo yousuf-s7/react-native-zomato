303871b43b3d54a944103fb6a998698de58745e0
yousuf-s7 <71456157+yousuf-s7@users.noreply.github.com>
2020-09-22 22:42:32 +0530

    Update zomato.js

--- Api/zomato.js
+++ Api/zomato.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://developers.zomato.com/api/v2.1",
   headers: {
-    "user-key": "01f956d814b9aaf104d657f89934e976",
+    "user-key": "secret-key",
     "content-type": "application/json",
   },
 });
